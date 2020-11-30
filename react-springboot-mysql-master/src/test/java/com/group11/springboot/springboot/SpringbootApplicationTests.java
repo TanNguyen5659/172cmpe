9f962a015f6c7e8edb13dea05ffc220b0ed2628b
Tan Nguyen <tannguyen5659@gmail.com>
2020-11-29 22:06:45 -0800

    created .jar file for webpage hosting

--- react-springboot-mysql-master/src/test/java/com/group11/springboot/springboot/SpringbootApplicationTests.java
+++ react-springboot-mysql-master/src/test/java/com/group11/springboot/springboot/SpringbootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mukundmadhav.springboot.springboot;
+package com.group11.springboot.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
